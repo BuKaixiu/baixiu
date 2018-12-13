0c0225e955e4c3e6903169ec8953454751962a03
bukaixiu <bukaixiu@gmail.com>
2018-12-13 15:27:51 +0800

    Add function 'connectDatabase and bx_add.'

--- functions.php
+++ functions.php
@@ -25,16 +25,26 @@ function bx_get_current_user () {
 }
 
 /**
+ * connectDatabase
+ * @Author   zhengkai
+ * @DateTime 2018-12-13T15:02:18+0800
+ * @return   [type]                   [description]
+ */
+function connectDatabase() {
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	if (!$conn) {
+		exit('<h1>连接数据库失败</h1>');
+	}
+	return $conn;
+}
+/**
  * 通过一个数据库查询获取多条数据
  * @Author   zhengkai
  * @DateTime 2018-12-11T14:37:35+0800
  * @return $resultcuu
  */
 function bx_fetch ($sql) {
-	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-	if (!$conn) {
-		exit('<h1>连接数据库失败</h1>');
-	}
+	$conn = connectDatabase();
 	$query = mysqli_query($conn,$sql);
 	if (!$query) {
 		// 查询失败
@@ -57,4 +67,15 @@ function bx_fetch ($sql) {
 function bx_fetch_one($sql) {
 	$rows = bx_fetch($sql);
 	return isset($rows[0]) ? $rows[0] : null;
+}
+
+function bx_add($sql) {
+	$conn = connectDatabase();
+	$query = mysqli_query($conn, $sql);
+	if (!$query) {
+		return false;
+	}
+	mysqli_free_result($query);
+	mysqli_close($conn);
+	return true;
 }
